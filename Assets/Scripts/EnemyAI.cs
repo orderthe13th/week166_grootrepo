c9e7a2fb9985f8c5a80b5358190f77ae36f98abd
akasharkxx <akash.jaiswar41@gmail.com>
2020-09-22 21:20:31 +0530

    no further update

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private float speed, lookTime;
+
+    private int directionMultiplier;
+    private float elapsed;
+    private bool isMovingRight;
+
+    private void Start()
+    {
+        directionMultiplier = 1;
+        isMovingRight = true;
+        elapsed = 0;
+    }
+
+    private void Update()
+    {
+        elapsed += Time.deltaTime;
+        if(elapsed >= lookTime)
+        {
+            isMovingRight = !isMovingRight;
+            Debug.Log("Flipping me");
+            FlipMe();
+            elapsed = 0;
+        }
+        transform.Translate(transform.right * speed * Time.deltaTime);
+    }
+
+    private void FlipMe()
+    {
+        if (isMovingRight)
+        {
+            directionMultiplier = 1;
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        else
+        {
+            directionMultiplier = -1;
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const string HORIZONTAL_AXIS = "Horizontal";
+
+    [SerializeField] private float moveSpeed;
+
+    private CharacterController2D characterControllerOnPlayer;
+    private Animator animatorOnPlayer;
+    private float horizontalMove;
+    private bool isJump, isCrouch;
+
+    private void Start()
+    {
+        characterControllerOnPlayer = GetComponent<CharacterController2D>();
+        animatorOnPlayer = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw(HORIZONTAL_AXIS) * moveSpeed;
+        Debug.Log(horizontalMove);
+        animatorOnPlayer.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isJump = true;
+            SetJumpBool(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isCrouch = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            isCrouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        SetJumpBool(false);
+    }
+
+    private void SetJumpBool(bool value)
+    {
+        animatorOnPlayer.SetBool("isJumping", value);
+    }
+
+    private void FixedUpdate()
+    {
+        characterControllerOnPlayer.Move(horizontalMove * Time.deltaTime, isCrouch, isJump);
+        isJump = false;
+    }
+}
